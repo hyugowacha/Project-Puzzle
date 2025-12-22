5748429d25fc0f2366b2ec4d479393581c5d18c0
hyugowacha <105649884+hyugowacha@users.noreply.github.com>
2025-12-22 15:38:52 +0900

    [test] 마우스 드래그 시 좌표 확인 테스트

--- Assets/1. Scripts/Gem.cs
+++ Assets/1. Scripts/Gem.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Gem : MonoBehaviour
 {
@@ -7,4 +8,19 @@ public class Gem : MonoBehaviour
         Debug.Log($"Clicked: {transform.position.x}, {transform.position.y}");
     }
 
+    private void OnMouseDrag()
+    {
+        Debug.Log(Mouse.current.position);
+    }
+
+    private void OnMouseUp()
+    {
+        Vector2 screenPos = Mouse.current.position.ReadValue();
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(
+            new Vector3(screenPos.x, screenPos.y, 0f)
+        );
+
+        Debug.Log($"Mouse Up World Pos: {worldPos}");
+    }
 }
