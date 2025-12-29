bfd36cce45c5923472e16321d14f2f42a5967a63
ChoiEuna <105649884+hyugowacha@users.noreply.github.com>
2025-12-29 18:19:02 +0900

    [refactor] boardmanager, spawner 리팩토링, 풀링 스크립트 gempool 추가

--- Assets/1. Scripts/BoardManager.cs
+++ Assets/1. Scripts/BoardManager.cs
@@ -6,19 +6,15 @@ public class BoardManager : MonoBehaviour
     public const int Width = 8;
     public const int Height = 8;
 
-    public Gem[,] board = new Gem[Width, Height];
+    public Gem[,] gemBoard = new Gem[Width, Height];
 
     public Gem[] gemPrefabs;
 
+    [SerializeField] private GemSpawner spawner;
+
     private void Start()
     {
-        for (int x = 0; x < Width; x++)
-        {
-            for (int y = 0; y < Height; y++)
-            {
-                SpawnGem(x, y);           
-            }
-        }
+        spawner.FillBoard(Width, Height);
     }
 
     public Vector3 Cell(int x, int y)
@@ -29,68 +25,5 @@ public class BoardManager : MonoBehaviour
         );
     }
 
-    void SpawnGem(int x, int y)
-    {
-        GemType? banH = BanHorizon(x, y); 
-        GemType? banV = BanVertical(x, y); 
-
-        int index = PickSafe(banH,banV);
-
-        Gem gem = Instantiate(gemPrefabs[index]);
-        gem.transform.position = Cell(x, y);
-        board[x, y] = gem;
-    }
-
-    GemType? BanHorizon(int x, int y)
-    {
-        if (x < 2)
-        {
-            return null;
-        }
-
-        var a = board[x - 1, y];
-        var b = board[x - 2, y];
-
-        if (a == null || b == null)
-        {
-            return null;
-        }
-
-        return a.type == b.type ? a.type : null;
-    }
-
-    GemType? BanVertical(int x, int y)
-    {
-        if (y < 2)
-        {
-            return null;
-        }
-
-        var a = board[x, y - 1];
-        var b = board[x, y - 2];
-
-        if (a == null || b == null)
-        {
-            return null;
-        }
-
-        return a.type == b.type ? a.type : null;
-    }
-
-    int PickSafe(GemType? banH, GemType? banV)
-    {
-        for (int i = 0; i < 20; i++)
-        {
-            int index = UnityEngine.Random.Range(0,gemPrefabs.Length);
-            GemType t = gemPrefabs[index].type;
-
-            if (t == banH) continue;
-            if(t==banV ) continue;
-
-            return index;
-        }
-
-        return UnityEngine.Random.Range(0, gemPrefabs.Length);
-    }
 
 }
--- Assets/1. Scripts/GemSpawner.cs
+++ Assets/1. Scripts/GemSpawner.cs
@@ -2,15 +2,83 @@ using UnityEngine;
 
 public class GemSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private BoardManager board;
+    [SerializeField] private Gem[] gemPrefabs;
+    private int maxTry = 20;
+
+    public void FillBoard(int width, int height)
     {
-        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                SpawnGem(x, y);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnGem(int x, int y)
     {
-        
+        GemType? banH = BanHorizon(x, y);
+        GemType? banV = BanVertical(x, y);
+
+        int index = PickSafe(banH, banV);
+
+        Gem gem = Instantiate(gemPrefabs[index]);
+        gem.transform.position = board.Cell(x, y);
+        board.gemBoard[x, y] = gem;
     }
+
+    GemType? BanHorizon(int x, int y)
+    {
+        if (x < 2)
+        {
+            return null;
+        }
+
+        var a = board.gemBoard[x - 1, y];
+        var b = board.gemBoard[x - 2, y];
+
+        if (a == null || b == null)
+        {
+            return null;
+        }
+
+        return a.type == b.type ? a.type : null;
+    }
+
+    GemType? BanVertical(int x, int y)
+    {
+        if (y < 2)
+        {
+            return null;
+        }
+
+        var a = board.gemBoard[x, y - 1];
+        var b = board.gemBoard[x, y - 2];
+
+        if (a == null || b == null)
+        {
+            return null;
+        }
+
+        return a.type == b.type ? a.type : null;
+    }
+
+    int PickSafe(GemType? banH, GemType? banV)
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            int index = UnityEngine.Random.Range(0, gemPrefabs.Length);
+            GemType t = gemPrefabs[index].type;
+
+            if (t == banH) continue;
+            if (t == banV) continue;
+
+            return index;
+        }
+
+        return UnityEngine.Random.Range(0, gemPrefabs.Length);
+    }
+
 }
